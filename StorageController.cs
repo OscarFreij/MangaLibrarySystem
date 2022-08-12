b5d6fe83452682b50f52112dd5a258db43855a8a
OscarFreij <otg020313@gmail.com>
2022-08-12 16:36:12 +0200

    Add StorrageController
    Added abillity to read bytes from file

---
+++ StorageController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaLibrarySystem
+{
+    public static class StorageController
+    {
+        public static string StorageDirectory;
+
+        public static async Task<byte[]> ReadFileBytes(FileType fileType, string fileId)
+        {
+            string subDirectory = string.Empty;
+            switch(fileType)
+            {
+                case FileType.Image:
+                    subDirectory = "/image";
+                    break;
+
+                case FileType.Data:
+                    subDirectory = "/data";
+                    break;
+            }
+
+            byte[] fileBytes = null;
+            using (FileStream fs = File.Open(StorageDirectory + subDirectory + "/" + fileId, FileMode.Open))
+            {
+                fileBytes = new byte[fs.Length];
+                await fs.ReadAsync(fileBytes, 0, (int)fs.Length);
+            }
+
+            return fileBytes;
+        }
+
+        public enum FileType
+        {
+            Image = 0,
+            Data = 1
+        }
+    }
+}
