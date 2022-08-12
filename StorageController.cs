0b7d3d0fa39ee8408957023470b3fa125f032085
OscarFreij <otg020313@gmail.com>
2022-08-12 16:36:43 +0200

    Add ByteToImage function
    Added function to turn byte[] into bitmap

--- StorageController.cs
+++ StorageController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -33,6 +34,16 @@ namespace MangaLibrarySystem
             }
 
             return fileBytes;
+        }
+
+        public static Bitmap ByteToImage(byte[] blob)
+        {
+            MemoryStream mStream = new MemoryStream();
+            byte[] pData = blob;
+            mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+            Bitmap bm = new Bitmap(mStream, false);
+            mStream.Dispose();
+            return bm;
         }
 
         public enum FileType
